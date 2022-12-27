f3e6770c83f9b64160977a8aaa12526c5a38ae85
Miguel Galicia <migel.gt@outlook.com>
2022-12-27 08:42:24 -0600

    add main routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-	  <h1>Tataa </h1>
-      </header>
+     <Routes>
+      <Route
+	  path="/"
+	  element=<Outlet/>
+      >
+         <Route
+	   path="/"
+           element=<div>Login</div>
+	 />
+         <Route
+	   path="memories"
+           element=<div>Memories List</div>
+	 />
+      </Route>
+     </Routes>
     </div>
   );
 }
