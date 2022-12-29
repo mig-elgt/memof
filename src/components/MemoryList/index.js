79a9e360ab571f9632fafbb77c844b4fc78f5275
Miguel Galicia <migel.gt@outlook.com>
2022-12-29 01:23:57 -0600

    design memories component

--- src/components/MemoryList/index.js
+++ src/components/MemoryList/index.js
@@ -2,7 +2,7 @@ import MemoryListItem from "../MemoryListItem";
 import ValidateKeyAndSendEmail from "../ValidateKeyAndSendEmail";
 import { useEffect, useState, useRef } from "react";
 import { getMemories } from "../../services/service.js";
-import { Button, VStack, Wrap } from '@chakra-ui/react'
+import { Button, VStack, Wrap, Text, Icon } from '@chakra-ui/react'
 
 const MemoryList = (props) => {
   const [memories, setMemoryList] = useState([]);
@@ -57,8 +57,18 @@ const MemoryList = (props) => {
   return (
     <div>
       <ValidateKeyAndSendEmail isModalOpen={isModalOpen} handleOnClose={handleOnClose} handlePublicKey={handlePublicKey}/>
-      <Wrap justify='center'>
+      <Wrap justify='center' pt={5} pb={5}>
       <VStack >
+      <Text fontSize='2xl'>
+	  Chaparrita 
+	  <Icon viewBox='0 0 512 512' color='red.500' mx={2}>
+		  <path
+		    fill='currentColor'
+		    d='M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z'
+		  />
+		</Icon>
+	  & Chaparrito</Text>
+      <Text fontSize='2xl'>Our Memories</Text>
       {memories.map((memory, i) => {
         return (
           <div key={memory.id}>
--- src/components/MemoryListItem/index.js
+++ src/components/MemoryListItem/index.js
@@ -1,14 +1,23 @@
-import { Image, Box } from '@chakra-ui/react'
+import { Image, Box, Stack, VStack, Center, Text } from '@chakra-ui/react';
+import Moment from 'react-moment';
 
 const MemoryListItem = (props) => {
   return (
      <div>
-	  <span>{props.memory.date}</span>
-	  <br/>
-	  <span>{props.memory.title}</span>
-	  <Box boxSize='100%'>
-   		<Image src={props.memory.image_url} boxSize='100%' />
-          </Box>
+	  <Stack direction='row' px={5}>
+	     <VStack>
+		<Text fontSize='md'><Moment format="MMM" withTitle>{props.memory.date}</Moment></Text>
+	        <Text fontSize='xl'><Moment format='DD'>{props.memory.date}</Moment></Text>
+	  	<Center w='3px' bg='purple.500' h='100%'>
+		</Center>
+	     </VStack>
+	     <Stack>
+	        <Box boxSize='100%'>
+                  <Image src={props.memory.image_url} boxSize='100%' />
+	        </Box>
+	        <Text fontSize='sm'>{props.memory.title}</Text>
+	     </Stack>
+	  </Stack>
      </div>
   );
 };
