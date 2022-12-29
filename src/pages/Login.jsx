a5c5c40e84d761eaecb5babef1b8afab6cd995b2
Miguel Galicia <migel.gt@outlook.com>
2022-12-28 21:58:58 -0600

    add loging validations

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { login } from "../services/service";
 import { useNavigate } from "react-router-dom";
-import {FormControl, Input, Button, Container, Wrap, VStack} from '@chakra-ui/react'
+import {FormControl, Input, Button, Container, Wrap, VStack, useToast} from '@chakra-ui/react'
 import {ArrowForwardIcon} from '@chakra-ui/icons'
 
 export default function Login(props) {
@@ -13,6 +13,7 @@ export default function Login(props) {
   const [error, setError] = useState(null);
   const [isLogIn, setIsLogIn] = useState(false);
   const navigate = useNavigate();
+  const toast = useToast();
 
   function handleInputChange(event) {
     const { name, value } = event.target;
@@ -29,12 +30,14 @@ export default function Login(props) {
     login(creds).then((res) => {
       setIsLogIn(false)
       if (res.status === 403) {
+	toast({ description: 'Invalid Credentials', status: 'error'})
 	return setError({message: "Invalid Credentials"})
       }
       localStorage.setItem("access_token", res.data.token)
       navigate("/memories");
     }).catch((err) => {
 	console.log(err)
+	toast({ description: 'Something went wrong', status: 'error'})
         setIsLogIn(false)
     });
   }
