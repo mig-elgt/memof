bb392d1f1fec6693e7e2b6896de4d87042427b2c
Marijo <marijo.torre.03@gmail.com>
2023-03-14 17:52:11 -0600

    add breaks

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
 import { login } from "../services/service";
 import { useNavigate } from "react-router-dom";
-import { Text, FormControl, Input, Button, Wrap, VStack, useToast} from '@chakra-ui/react'
+import {
+  Text,
+  FormControl,
+  Input,
+  Button,
+  Wrap,
+  VStack,
+  useToast,
+} from "@chakra-ui/react";
 
 export default function Login(props) {
   const [form, setForm] = useState({
@@ -21,57 +29,83 @@ export default function Login(props) {
   function handleFormSubmission(event) {
     event.preventDefault();
     const creds = {
-	    user: username,
-      	    pass: password,
+      user: username,
+      pass: password,
     };
-    setIsLogIn(true)
-    login(creds).then((res) => {
-      setIsLogIn(false)
-      if (res.status === 403) {
-	toast({ description: 'Invalid Credentials', status: 'error'})
-	return
-      }
-      localStorage.setItem("access_token", res.data.token)
-      navigate("/memories");
-    }).catch((err) => {
-	console.log(err)
-	toast({ description: 'Something went wrong', status: 'error'})
-        setIsLogIn(false)
-    });
+    setIsLogIn(true);
+    login(creds)
+      .then((res) => {
+        setIsLogIn(false);
+        if (res.status === 403) {
+          toast({ description: "Invalid Credentials", status: "error" });
+          return;
+        }
+        localStorage.setItem("access_token", res.data.token);
+        navigate("/memories");
+      })
+      .catch((err) => {
+        console.log(err);
+        toast({ description: "Something went wrong", status: "error" });
+        setIsLogIn(false);
+      });
   }
 
   return (
-	  <Wrap justify='center' px="20px">
-	  <VStack>
-	          <Text textAlign='center' fontSize='4xl' fontWeight={'bold'}>Welcome to Memories</Text>
-	          <Text textAlign='center' fontSize='md'>The online platform to go back to the past and remember our life together</Text>
-	          <Text  textAlign='center' fontSize='md' fontWeight={'bold'}>Are you ready? </Text>
-			  <Text  textAlign='center' fontSize='sm'>Enter your credentials and take a look to your memories with your Chaparrito💖</Text>
+    <Wrap justify="center" px="20px">
+      <VStack>
+        <Text textAlign="center" fontSize="4xl" fontWeight={"bold"}>
+          Welcome to Memories
+        </Text>
+        <Text textAlign="center" fontSize="md">
+          The online platform to go back to the past and remember our life
+          together
+        </Text>
+		<br></br>
+        <Text textAlign="center" fontSize="md" fontWeight={"bold"}>
+          Are you ready?{" "}
+        </Text>
+        <Text textAlign="center" fontSize="sm">
+          Enter your credentials and take a look to your memories <br></br>with
+          your Chaparrito💖
+        </Text>
 
-		  <form onSubmit={handleFormSubmission}>
-		      <FormControl isRequired mt={5}>
-			  <Input id="input-username" type='text' mb="15px"
-			  name="username"
-			  placeholder='Username'
-			  value={username}
-			  onChange={handleInputChange}
-			  required/>
-			</FormControl>
-			<FormControl isRequired>
-			  <Input id="input-password" type='password' mb="15px"
-			  name="password"
-			  placeholder='Password'
-			  value={password}
-			  onChange={handleInputChange}
-			  required
-			  />
+        <form onSubmit={handleFormSubmission}>
+          <FormControl isRequired mt={5}>
+            <Input
+              id="input-username"
+              type="text"
+              mb="15px"
+              name="username"
+              placeholder="Username"
+              value={username}
+              onChange={handleInputChange}
+              required
+            />
+          </FormControl>
+          <FormControl isRequired>
+            <Input
+              id="input-password"
+              type="password"
+              mb="15px"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={handleInputChange}
+              required
+            />
 
-		         <Button mt={5} isLoading={isLogIn} className="button__submit" type="submit" colorScheme='purple'>
-		          Log In
-		         </Button>
-		       </FormControl>
-		  </form>
-	     </VStack>
-	  </Wrap>
+            <Button
+              mt={5}
+              isLoading={isLogIn}
+              className="button__submit"
+              type="submit"
+              colorScheme="purple"
+            >
+              Log In
+            </Button>
+          </FormControl>
+        </form>
+      </VStack>
+    </Wrap>
   );
 }
