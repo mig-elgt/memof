2c54cbebfc539b06740a9f92fec9135a1904eb6c
Marijo <marijo.torre.03@gmail.com>
2023-03-14 19:09:17 -0600

    add box to memory tile, change color

--- src/components/MemoryList/index.js
+++ src/components/MemoryList/index.js
@@ -2,7 +2,7 @@ import MemoryListItem from "../MemoryListItem";
 import ValidateKeyAndSendEmail from "../ValidateKeyAndSendEmail";
 import { useEffect, useState, useRef } from "react";
 import { getMemories } from "../../services/service.js";
-import { Button, VStack, Wrap, Text, Icon } from '@chakra-ui/react'
+import { Button, VStack, Wrap, Text, Icon } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 const MemoryList = (props) => {
@@ -14,84 +14,88 @@ const MemoryList = (props) => {
 
   let refCount = useRef(1);
 
-  useEffect(() => { 
+  useEffect(() => {
     if (localStorage.getItem("access_token") === null) {
       navigate("/");
-      return
+      return;
     }
-    getMemories(refCount.current, 4,publicKey)
+    getMemories(refCount.current, 4, publicKey)
       .then((resp) => {
-	  if (resp.status === 200) {
-	     setMemoryList(resp.data.memories)
-	  }
+        if (resp.status === 200) {
+          setMemoryList(resp.data.memories);
+        }
       })
-      .catch((err) => console.log(err))
+      .catch((err) => console.log(err));
   }, []);
 
   const onLoadMoreMemories = (e) => {
     refCount.current++;
-    setIsLoading(true)
-    getMemories(refCount.current,4,publicKey)
+    setIsLoading(true);
+    getMemories(refCount.current, 4, publicKey)
       .then((resp) => {
-	  console.log(resp)
-	  if (resp.status === 200) {
-	     let newMemories = [...memories]
-	     newMemories.push(...resp.data.memories)
-	     setMemoryList(newMemories)
-	  } else {
-             refCount.current--;
-	     setIsModalOpen(true)
-	  }
-    	  setIsLoading(false)
+        console.log(resp);
+        if (resp.status === 200) {
+          let newMemories = [...memories];
+          newMemories.push(...resp.data.memories);
+          setMemoryList(newMemories);
+        } else {
+          refCount.current--;
+          setIsModalOpen(true);
+        }
+        setIsLoading(false);
       })
-      .catch((err) => { 
-	 console.log(err)
-    	 setIsLoading(false)
-      })
-  }
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  };
 
   const handleOnClose = () => {
-    setIsModalOpen(false)
+    setIsModalOpen(false);
   };
 
   const handlePublicKey = (key) => {
-    console.log("got public key", key)
-    setIsModalOpen(false)
-    setPublicKey(key)
+    console.log("got public key", key);
+    setIsModalOpen(false);
+    setPublicKey(key);
   };
- 
 
   return (
     <div>
-      <ValidateKeyAndSendEmail isModalOpen={isModalOpen} handleOnClose={handleOnClose} handlePublicKey={handlePublicKey}/>
-      <Wrap justify='center' pt={5} pb={5}>
-      <VStack >
-      <Text fontSize='2xl'>
-	  Chaparrita 
-	  <Icon viewBox='0 0 512 512' color='red.500' mx={2}>
-		  <path
-		    fill='currentColor'
-		    d='M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z'
-		  />
-		</Icon>
-	  & Chaparrito</Text>
-      <Text fontSize='2xl'>Our Memories</Text>
-      {memories.map((memory, i) => {
-        return (
-          <div key={memory.id}>
-            <MemoryListItem memory={memory}/>
-          </div>
-        );
-      })}
-	  <Button
-	    isLoading={isLoading}
-	    loadingText='Loading'
-	    colorScheme='purple'
-	    onClick={onLoadMoreMemories}
-	  >
-	  Load more
-	  </Button>
-      </VStack>
+      <ValidateKeyAndSendEmail
+        isModalOpen={isModalOpen}
+        handleOnClose={handleOnClose}
+        handlePublicKey={handlePublicKey}
+      />
+      <Wrap justify="center" pt={5} pb={5}>
+        <VStack>
+          <Text fontSize="2xl">
+            Chaparrita
+            <Icon viewBox="0 0 512 512" color="purple.500" mx={2}>
+              <path
+                fill="currentColor"
+                d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"
+              />
+            </Icon>
+            Chaparrito
+          </Text>
+          <Text fontSize="2xl">Our Memories</Text>
+          {memories.map((memory, i) => {
+            return (
+              <div key={memory.id}>
+                <MemoryListItem memory={memory} />
+              </div>
+            );
+          })}
+          <Button
+            isLoading={isLoading}
+            loadingText="Loading"
+            colorScheme="purple"
+            onClick={onLoadMoreMemories}
+          >
+            Load more
+          </Button>
+        </VStack>
       </Wrap>
     </div>
   );
--- src/components/MemoryListItem/index.js
+++ src/components/MemoryListItem/index.js
@@ -1,24 +1,31 @@
-import { Image, Box, Stack, VStack, Center, Text } from '@chakra-ui/react';
-import Moment from 'react-moment';
+import { Image, Box, Stack, VStack, Center, Text } from "@chakra-ui/react";
+import Moment from "react-moment";
 
 const MemoryListItem = (props) => {
   return (
-     <div>
-	  <Stack direction='row' px={5}>
-	     <VStack>
-		<Text fontSize='md'><Moment format="MMM" withTitle>{props.memory.date}</Moment></Text>
-	        <Text fontSize='xl'><Moment format='DD'>{props.memory.date}</Moment></Text>
-	  	<Center w='3px' bg='purple.500' h='100%'>
-		</Center>
-	     </VStack>
-	     <Stack>
-	        <Text fontSize='sm'>{props.memory.title}</Text>
-	        <Box boxSize='100%'>
-                  <Image src={props.memory.image_url} boxSize='100%' />
-	        </Box>
-	     </Stack>
-	  </Stack>
-     </div>
+    <div>
+      <Stack direction="row" px={5}>
+        <VStack>
+          <Text fontSize="xl" fontWeight={"bold"}>
+            <Moment format="MMM" withTitle>
+              {props.memory.date}
+            </Moment>
+          </Text>
+          <Text fontSize="xl">
+            <Moment format="DD">{props.memory.date}</Moment>
+          </Text>
+          <Center w="3px" bg="purple.500" h="100%"></Center>
+        </VStack>
+        <Stack>
+          <Box borderRadius='md' bg='purple.500' color='white' px={4}>
+            <Text fontSize="md" fontWeight={'bold'}>{props.memory.title}</Text>
+          </Box>
+          <Box boxSize="100%">
+            <Image src={props.memory.image_url} boxSize="100%" />
+          </Box>
+        </Stack>
+      </Stack>
+    </div>
   );
 };
 
