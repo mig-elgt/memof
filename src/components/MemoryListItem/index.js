7a4ecbbb437643b1b3331f05ce62d5693b88632f
Marijo <marijo.torre.03@gmail.com>
2023-03-16 19:29:39 -0600

    add card

--- src/components/MemoryListItem/index.js
+++ src/components/MemoryListItem/index.js
@@ -5,7 +5,12 @@ import {
   VStack,
   Center,
   Text,
-  Circle, Card, CardHeader, CardBody, CardFooter 
+  Circle,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Heading,
 } from "@chakra-ui/react";
 import Moment from "react-moment";
 
@@ -25,6 +30,23 @@ const MemoryListItem = (props) => {
           <Center w="3px" bg="purple.500" h="100%"></Center>
         </VStack>
         <Stack>
+          <Card maxW="sm">
+            <CardBody>
+              <Image
+                src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                alt="Green double couch with wooden legs"
+                borderRadius="lg"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">Living room Sofa</Heading>
+                <Text>
+                  This sofa is perfect for modern tropical spaces, baroque
+                  inspired spaces, earthy toned spaces and for people who love a
+                  chic design with a sprinkle of vintage design.
+                </Text>
+              </Stack>
+            </CardBody>
+          </Card>
           <Box borderRadius="md" bg="purple.500" color="white" px={4}>
             <Text fontSize="md" fontWeight={"bold"}>
               {props.memory.title}
