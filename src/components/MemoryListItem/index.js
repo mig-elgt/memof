aa44bc1a47fabdd8b8a016944a19e9335ed40018
Marijo <marijo.torre.03@gmail.com>
2023-03-17 17:51:52 -0600

    fix image border, add bottom margin to image title

--- src/components/MemoryListItem/index.js
+++ src/components/MemoryListItem/index.js
@@ -13,6 +13,7 @@ import {
   CardBody,
   CardFooter,
   Heading,
+  Divider,
 } from "@chakra-ui/react";
 import Moment from "react-moment";
 
@@ -33,14 +34,21 @@ const MemoryListItem = (props) => {
         </VStack>
         <Box>
           <Card maxW="md">
-            <CardHeader bg="whiteAlpha.200" borderRadius="xl">
-              <Box borderRadius="md" bg="purple.500" color="white" px={4}>
-                <Text fontSize="md" fontWeight={"bold"}>
+            <CardHeader bg="whiteAlpha.100" borderRadius="xl">
+              <Box
+                borderRadius="md"
+                bg="purple.500"
+                color="white"
+                px={4}
+                mb={4}
+                w="100%"
+              >
+                <Text fontSize="md" fontWeight={"bold"} >
                   {props.memory.title}
                 </Text>
               </Box>
               <Box boxSize="100%">
-                <Image src={props.memory.image_url} boxSize="100%" />
+                <Image src={props.memory.image_url} boxSize="100%" borderRadius='md'/>
               </Box>
             </CardHeader>
           </Card>
